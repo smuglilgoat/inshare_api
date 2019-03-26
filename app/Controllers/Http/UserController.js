bcdf9d5df4bc0b4b52192a83f59c67d1aac4f30b
MOUSSAOUI Ahmed <k.mame2810@gmail.com>
2019-03-26 21:02:56 +0100

    auth-fix

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,39 +3,18 @@
 const User = use('App/Models/User');
 
 class UserController {
-	async inscription({ request, auth, response }) {
+	async inscription({ request, auth }) {
 		const userData = request.only([ 'username', 'email', 'password' ]);
+		const user = await User.create(userData);
+		const token = await auth.generate(user);
 
-		try {
-			const user = await User.create(userData);
-			const token = await auth.generate(user);
-
-			return response.json({
-				status: 'success',
-				data: token
-			});
-		} catch (error) {
-			return response.status(400).json({
-				status: 'error',
-				message: "Nous n'avons pas pu crée l'utilisateur, veuillez réessayer plus tard."
-			});
-		}
+		return token;
 	}
 
-	async connexion({ request, auth, response }) {
-		try {
-			const token = await auth.attempt(request.input('email'), request.input('password'));
+	async connexion({ request, auth }) {
+		const token = await auth.attempt(request.input('email'), request.input('password'));
 
-			return response.json({
-				status: 'success',
-				data: token
-			});
-		} catch (error) {
-			response.status(400).json({
-				status: 'error',
-				message: 'Email ou mot de passe invalide'
-			});
-		}
+		return token;
 	}
 }
 
--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -10,7 +10,6 @@ class UserSchema extends Schema {
 			table.string('username', 80).notNullable().unique();
 			table.string('email', 254).notNullable().unique();
 			table.string('password', 60).notNullable();
-			table.string('role');
 			table.timestamps();
 		});
 	}
